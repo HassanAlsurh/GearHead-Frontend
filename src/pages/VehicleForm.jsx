720caca930445641e2edb7ddfe6e9ccf172f9e54
Hasan Ali <hassanalsurh@gmail.com>
2026-08-09 11:18:44 +0300

    Completed the vehicle Creation Form

--- src/pages/VehicleForm.jsx
+++ src/pages/VehicleForm.jsx
@@ -1,12 +1,98 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
-import * as vehicleServices from '../services/vehicles'
+import { show } from '../services/vehicles'
 const VehicleForm = () => {
 
-    return (
-        <>
+      const { vehicleId } = useParams()
 
-        </>
+    const initialState = {
+        year: undefined,
+        make: undefined,
+        model: undefined,
+        mileage: undefined,
+        image: undefined
+    }
+
+    const [formData, setFormData] = useState(initialState)
+
+    const handleChange = (event) => {
+        setFormData({ ...formData, [event.target.name]: event.target.value })
+    }
+
+    const handleSubmit = (event) => {
+        evt.preventDefault()
+
+        // handle Submit here
+
+        setFormData(initialState)
+
+    }
+
+    useEffect(() => {
+        const fetchVehicle = async () => {
+            const vehicleData = await hootService.show(vehicleId)
+            setFormData(vehicleData)
+        }
+        if (vehicleId) fetchVehicle()
+    }, [vehicleId])
+
+    return (
+        <main className='card'>
+            <h1>{vehicleId ? 'Edit Vehicle' : 'New Vehicle' }</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='make-input'>Make</label>
+                <input
+                    required
+                    type='text'
+                    name='make'
+                    id='make-input'
+                    value={formData.make}
+                    onChange={handleChange}
+                />
+                <label htmlFor='model-input'>Model</label>
+                <input
+                    required
+                    type='text'
+                    name='model'
+                    id='model-input'
+                    value={formData.model}
+                    onChange={handleChange}
+                />
+                <label htmlFor='year-input'>Year</label>
+                <input
+                    required
+                    min={0}
+                    type='number'
+                    name='year'
+                    id='year-input'
+                    value={formData.year}
+                    onChange={handleChange}
+                />
+                <label htmlFor='mileage-input'>Mileage</label>
+                <input
+                    required
+                    min={0}
+                    type='number'
+                    name='mileage'
+                    id='mileage-input'
+                    value={formData.mileage}
+                    onChange={handleChange}
+                />
+                <label htmlFor='image-input'>Image</label>
+                <input
+                    required
+                    type='text'
+                    name='image'
+                    id='image-input'
+                    value={formData.image}
+                    onChange={handleChange}
+                />
+
+
+                <button type='submit'>SUBMIT</button>
+            </form>
+        </main>
     )
 }
 
