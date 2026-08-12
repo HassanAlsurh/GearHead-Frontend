339886e70aadb87e6e1518085b24abfb88e41e96
Hasan Ali <hassanalsurh@gmail.com>
2026-08-12 12:43:08 +0300

    Adding delete invite function

--- src/services/vehicles.js
+++ src/services/vehicles.js
@@ -104,6 +104,22 @@ const invite = async (vehicleId, formData) => {
   }
 }
 
+const deleteInvite = async (vehicleId, formData) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${vehicleId}/uninvite`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export {
   index,
   show,
@@ -112,5 +128,6 @@ export {
   update,
   sharedIndex,
   sharedShow,
-  invite
+  invite,
+  deleteInvite
 }
